86afb1af5c01a70057b0cf0db924b6564400ad66
tstuefe <bostei@stuefe.de>
2019-11-11 08:04:26 +0100

    Add LOM test results

---
+++ test/test_lom/run_test.sh
@@ -0,0 +1,8 @@
+
+#XOPTS="--num-classes=1 --num-loaders=8000"
+
+
+../jdk-original/bin/java -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:VitalsFile=original -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y $XOPTS
+../jdk-patched/bin/java -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:VitalsFile=patched-nolom -XX:-MetaspaceUseLOM -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y $XOPTS
+../jdk-patched/bin/java -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:VitalsFile=patched-lom -XX:+MetaspaceUseLOM  -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y $XOPTS
+
