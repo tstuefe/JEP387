78602fee69273cda13bcf7d68db6847d5aa0fdd5
tstuefe <bostei@stuefe.de>
2019-11-20 15:09:47 +0100

    Repeat micro tests (80000 dwarfs) with LOM and AlwaysPreTouch

--- test/test2/run_test.sh
+++ test/test2/run_test.sh
@@ -1,8 +1,10 @@
+EXT_OPTS=
 
-XOPTS="--num-classes=1 --num-loaders=8000"
+ALL_OPTS="-XX:+AlwaysPreTouch -Xmx100M -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 ${EXT_OPTS}"
 
+TEST_OPTS="--num-classes=1 --num-loaders=8000"
 
-../jdk-original/bin/java -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:VitalsFile=original -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y $XOPTS
-../jdk-patched/bin/java -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:VitalsFile=patched-standard -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y $XOPTS
-../jdk-patched/bin/java -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:VitalsFile=patched-aggressive -XX:MetaspaceReclaimStrategy=aggressive -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y $XOPTS
+../jdk-original/bin/java ${ALL_OPTS} -XX:VitalsFile=original -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y ${TEST_OPTS}
+../jdk-patched/bin/java  ${ALL_OPTS}  -XX:VitalsFile=patched-standard -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y ${TEST_OPTS}
+../jdk-patched/bin/java  ${ALL_OPTS}  -XX:VitalsFile=patched-aggressive -XX:MetaspaceReclaimStrategy=aggressive -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y ${TEST_OPTS}
 
