bf26f85c4d2d01336853b0980072720699b15449
tstuefe <thomas.stuefe@gmail.com>
2020-04-15 13:35:27 +0200

    Tests updated to use new example code

--- test/test-parallel-loaders/run_test.sh
+++ test/test-parallel-loaders/run_test.sh
@@ -1,4 +1,13 @@
-../jdk-original/bin/java -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:VitalsFile=original -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.ParallelLoaders -y
-../jdk-patched/bin/java -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:VitalsFile=patched-standard -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.ParallelLoaders -y
-../jdk-patched/bin/java -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:VitalsFile=patched-aggressive -XX:MetaspaceReclaimStrategy=aggressive -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.ParallelLoaders -y
+
+EXT_OPTS=
+
+ALL_OPTS="-XX:+AlwaysPreTouch -Xmx50M -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 ${EXT_OPTS}"
+
+
+
+../jdk-original/bin/java ${ALL_OPTS} -XX:VitalsFile=original -cp ../examples-1.0.jar de.stuefe.repros.metaspace.ParallelLoaders -y
+../jdk-patched/bin/java  ${ALL_OPTS}  -XX:VitalsFile=patched-standard -cp ../examples-1.0.jar de.stuefe.repros.metaspace.ParallelLoaders -y
+../jdk-patched/bin/java  ${ALL_OPTS}  -XX:VitalsFile=patched-aggressive -XX:MetaspaceReclaimStrategy=aggressive -cp ../examples-1.0.jar de.stuefe.repros.metaspace.ParallelLoaders -y
+
+
 
--- test/test1/run_test.sh
+++ test/test1/run_test.sh
@@ -3,7 +3,7 @@ EXT_OPTS=
 ALL_OPTS="-XX:+AlwaysPreTouch -Xmx50M -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 ${EXT_OPTS}"
 
 
-../jdk-original/bin/java ${ALL_OPTS} -XX:VitalsFile=original -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y
-../jdk-patched/bin/java  ${ALL_OPTS}  -XX:VitalsFile=patched-standard -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y
-../jdk-patched/bin/java  ${ALL_OPTS}  -XX:VitalsFile=patched-aggressive -XX:MetaspaceReclaimStrategy=aggressive -cp ../repros8-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders2 -y
+../jdk-original/bin/java ${ALL_OPTS} -XX:VitalsFile=original -cp ../examples-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders -y
+../jdk-patched/bin/java  ${ALL_OPTS}  -XX:VitalsFile=patched-standard -cp ../examples-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders -y
+../jdk-patched/bin/java  ${ALL_OPTS}  -XX:VitalsFile=patched-aggressive -XX:MetaspaceReclaimStrategy=aggressive -cp ../examples-1.0.jar de.stuefe.repros.metaspace.InterleavedLoaders -y
 
